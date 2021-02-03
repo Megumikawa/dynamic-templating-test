a04c4ecb7b4bc8df3762c36cc11e4ec2f8165554
Megumikawa <megumi19901016@gmail.com>
2021-02-03 15:02:14 +0100

    class activities

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
+let students = require('./exam-info')
 
 const app = express();
 
@@ -12,12 +13,22 @@ app.use(express.static('public'));
 
 // 1: in the home,list all the students who took the exam (list all the students)
 
-// ... Your code here
+app.get("/", (req, res) => {
+  for(let i = 0;students.length; i++){
+    students[i].name = students[i].name.toUpperCase
+  }
+    res.render('full-list.hbs', {students})
+});
+
+
+
+
 
 // 2: in the '/results' list all the students who passed the test and their score.
 // Also, students should be in descending order based on their score.
-
-// ... Your code here
+app.get("/results", (req, res) => {
+  res.render('results.hbs', {students})
+});
 
 app.listen(process.env.PORT, () =>
   console.log(`App running on ${process.env.PORT}.`)
